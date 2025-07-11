dce422fd5ba5f3d3834e536c8d301dc09f67913c
Shubham Midgule <shubham.midgule17@gmail.com>
2025-07-11 20:13:30 +0530

    File and Folder Strcture Completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,32 @@
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import { Route, Routes, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import MovieDetails from "./pages/MovieDetails";
+import SeatLayout from "./pages/SeatLayout";
+import MyBookings from "./pages/MyBookings";
+import Favourite from "./pages/favourite";
+import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
 
-export default App
+const App = () => {
+  const isAdminRoute = useLocation().pathname.startsWith("/admin");
+
+  return (
+    <>
+      <Toaster />
+      {!isAdminRoute && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/favourite" element={<Favourite />} />
+      </Routes>
+       {!isAdminRoute && <Footer />}
+    </>
+  );
+};
+
+export default App;
