defebab7ceea155de61b276558d8061d0ea83899
Shubham Midgule <shubham.midgule17@gmail.com>
2025-07-16 21:38:54 +0530

    Admin Dashboard is Added

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import {
+  ChartLineIcon,
+  DollarSignIcon,
+  PlayCircleIcon,
+  UsersIcon,
+} from "lucide-react";
+import { dummyDashboardData } from "../../assets/assets";
+import Loading from "../../components/Loading";
+import Title from "../../components/admin/Title";
+import BlurCircle from "../../components/BlurCircle";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  
+  const currency = import.meta.env.VITE_CURRENCY;
 
-export default Dashboard
+  const [dashboardData, setDashboardData] = useState({
+    totalBookings: 0,
+    totalRevenue: 0,
+    activeShows: 0,
+    totalUser: 0,
+  });
+
+  const [loading, setLoading] = useState(true);
+
+  const dashboardCards = [
+    {
+      title: "Total Bookings",
+      value: dashboardData.totalBookings || "0",
+      icon: ChartLineIcon,
+    },
+    {
+      title: "Total Revenue",
+      value: dashboardData.totalRevenue || "0",
+      icon: DollarSignIcon,
+    },
+    {
+      title: "Active Shows",
+      value: dashboardData.activeShows || "0",
+      icon: PlayCircleIcon,
+    },
+    {
+      title: "Total Users",
+      value: dashboardData.totalUser || "0",
+      icon: UsersIcon,
+    },
+  ];
+
+  const fetchDashboardData = async () => {
+    setDashboardData(dummyDashboardData);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchDashboardData();
+  }, []);
+
+  return !loading ? (
+    <>
+      <Title text1="Admin" text2="Dashboard" />
+
+      <div className="relative flex flex-wrap gap-4 mt-6">
+        <BlurCircle top="-100px" left="0" />
+        <div className="flex flex-wrap gap-4 w-full"></div>
+      </div>
+    </>
+  ) : (
+    <Loading />
+  );
+};
+
+export default Dashboard;
