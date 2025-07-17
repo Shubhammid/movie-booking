c136800d77430ff3de1734c2ecbb29c8551e75c5
Shubham Midgule <shubham.midgule17@gmail.com>
2025-07-17 21:39:51 +0530

    List Bookings Added

--- client/src/pages/admin/ListBookings.jsx
+++ client/src/pages/admin/ListBookings.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
 const ListBookings = () => {
+
+  const currency = import.meta.env.VITE_CURRENCY;
+  
+  const [bookings, setBookings] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <div>ListBookings</div>
   )
