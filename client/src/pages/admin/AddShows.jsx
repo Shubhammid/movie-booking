65f000826804169112dd89a83124a630cb76283c
Shubham Midgule <shubham.midgule17@gmail.com>
2025-07-18 19:34:16 +0530

    Add Shows Added

--- client/src/pages/admin/AddShows.jsx
+++ client/src/pages/admin/AddShows.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { dummyBookingData } from "../../assets/assets";
+import Loading from "../../components/Loading";
+import Title from "../../components/admin/Title";
 
 const AddShows = () => {
-  return (
-    <div>AddShows</div>
-  )
-}
+  const currency = import.meta.env.VITE_CURRENCY;
 
-export default AddShows
+  const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [dateTimeSelection, setDateTimeSelection] = useState({});
+  const [dateTimeInput, setDateTimeInput] = useState("");
+  const [showPrice, setShowPrice] = useState("");
+
+  const fetchNowPlayingMovies = async () => {
+    setNowPlayingMovies(dummyBookingData);
+  };
+
+  useEffect(() => {
+    fetchNowPlayingMovies();
+  }, []);
+
+  return nowPlayingMovies.length > 0 ? (
+    <>
+      <Title text1="Add" text2="Shows" />
+      <p className="mt-10 text-lg font-medium">Now Playing Movies</p>
+      <div className="overflow-x-auto pb-4">
+        <div className="group flex flex-wrap gap-4 mt-4 w-max">
+           {nowPlayingMovies.map((movie) => (
+              <div></div>
+           ))}
+        </div>
+      </div>
+    </>
+  ) : (
+    <Loading />
+  );
+};
+
+export default AddShows;
