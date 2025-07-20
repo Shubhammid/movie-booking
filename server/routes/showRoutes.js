fa52011965afb91d6a50d2d5e92bc64abb6623b2
Shubham Midgule <shubham.midgule17@gmail.com>
2025-07-20 18:06:36 +0530

    AddShow is Added

--- server/routes/showRoutes.js
+++ server/routes/showRoutes.js
@@ -4,5 +4,6 @@ import { addShow, getNowPlayingMovies } from "../controllers/showController.js";
 const showRouter = express.Router();
 
 showRouter.get("/now-playing", getNowPlayingMovies);
+showRouter.post("/add", addShow);
 
 export default showRouter;
