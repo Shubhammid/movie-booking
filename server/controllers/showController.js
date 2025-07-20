491512172eaca0ed0afbe864e5c0c4e08dab0d46
Shubham Midgule <shubham.midgule17@gmail.com>
2025-07-20 17:10:30 +0530

    Now Playing is Added

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -1,7 +1,20 @@
+import axios from "axios";
+import Movie from "../models/Movie.js";
+import Show from "../models/Show.js"
+
 export const getNowPlayingMovies = async (req, res) => {
-   try {
-    
-   } catch (error) {
-    
-   }
-}
+  try {
+    const { data } = await axios.get(
+      "https://api.themoviedb.org/3/movie/now_playing",
+      {
+        headers: { Authorization: `Bearer ${process.env.TMDB_API_KEY}` },
+      }
+    );
+
+    const movies = data.results;
+    res.json({ success: true, movies: movies });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
