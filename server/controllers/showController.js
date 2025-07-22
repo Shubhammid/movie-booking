dc6f5b25cf6c60ac15ea18057b67c9c675b540c2
Shubham Midgule <shubham.midgule17@gmail.com>
2025-07-22 18:27:30 +0530

    All API are tested using Postman

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -82,7 +82,7 @@ export const addShow = async (req, res) => {
 
 export const getShows = async (req, res) => {
   try {
-    const shows = await Show.find({ showDateTime: { $gte: new Date() }})
+    const shows = await Show.find({ showDateTime: { $gte: new Date() } })
       .populate("movie")
       .sort({ showDateTime: 1 });
     const uniqueShows = new Set(shows.map((show) => show.movie));
@@ -95,14 +95,21 @@ export const getShows = async (req, res) => {
 
 export const getShow = async (req, res) => {
   try {
-    const {movieId} = req.params;
-    const shows = await Show.find({ movie: movieId, showDateTime: { $gte: new Date() }})
+    const { movieId } = req.params;
+    const shows = await Show.find({
+      movie: movieId,
+      showDateTime: { $gte: new Date() },
+    });
     const movie = await Movie.findById(movieId);
     const dateTime = {};
     shows.forEach((show) => {
-      
-    })
-    res.json({ success: true, shows: Array.from(uniqueShows) });
+      const date = show.showDateTime.toISOString().split("T")[0];
+      if (!dateTime[date]) {
+        dateTime[date] = [];
+      }
+      dateTime[date].push({ time: show.showDateTime, showId: show._id });
+    });
+    res.json({ success: true, movie, dateTime });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
