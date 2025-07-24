a4e4afe4e675eb6409db729364e04e1f923d5243
Shubham Midgule <shubham.midgule17@gmail.com>
2025-07-24 21:30:47 +0530

    Fix typos in HeroSection component

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
     <div className="flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url('/backgroundImage.png')] bg-cover bg-center h-screen">
       <img src={assets.marvelLogo} alt="" className="max-h-11 lg:h-11 mt-20" />
       <h1 className="text-5xl md:text-[70px] md:leading-18 font-semibold max-w-110">
-        Guardians <br /> of the Galaxy
+        Guardians <br /> of the Galaxies
       </h1>
 
       <div className="flex items-center gap-4 text-gray-300">
@@ -29,7 +29,7 @@ const HeroSection = () => {
         onClick={() => navigate("/movies")}
         className="flex items-center gap-1 px-6 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer"
       >
-        Explore Movies
+        Explore Movie
         <ArrowRight className="w-5 h-5" />
       </button>
     </div>
